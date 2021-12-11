aec950f2fa50c23d87835391fb1206313164e9da
Dell <dell@DESKTOP-NLH0UUR>
2021-12-11 11:51:14 +0100

    End of section5

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import Activity from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
+
+interface IProps {
+  activities: Activity[];
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelSelectActivity: () => void;
+  editMode: boolean;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  handleCreateOrEdit:(activity: Activity) => void;
+  handleDelete: (id: string) => void;
+}
+export default function ActivityDashboard({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelSelectActivity,
+  editMode,
+  openForm,
+  closeForm,
+  handleCreateOrEdit,
+  handleDelete
+}: IProps) {
+  return (
+    <Grid>
+      <Grid.Column width="10">
+        <ActivityList activities={activities} selectActivity={selectActivity} handleDelete={handleDelete} />
+      </Grid.Column>
+      <Grid.Column width="6">
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
+            cancelSelectedActivity={cancelSelectActivity}
+            openForm={openForm}
+          />
+        )}
+        {editMode && 
+        <ActivityForm closeForm={closeForm} activity={selectedActivity} handleCreateOrEdit={handleCreateOrEdit}/>
+        }
+      </Grid.Column>
+    </Grid>
+  );
+}
